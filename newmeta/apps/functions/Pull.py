6513e2770fc1b64029fa878f78898b17fa622bc2
Gary Zheng <garydzheng@gmail.com>
2015-08-16 21:40:53 -0400

    :(

--- newmeta/apps/functions/Pull.py
+++ newmeta/apps/functions/Pull.py
@@ -64,8 +64,70 @@ def downloadData(version, gamemode, region):
                 print "~ " + str(e)
                 continue
 
+def downloadData2(version, gamemode, region):
+
+    gamemode = gamemode.upper()
+    region = region.upper()
+
+    matchIDs = getMatchIDs(version, gamemode, region)
+
+    while matchIDs:
+        print matchIDs
+        numMatches = len(matchIDs)
+        errorMatches = []
+
+        for i in range(numMatches):
+
+            print "Processing match {i} / {total}".format(i=i,total=numMatches),
+
+            mid = matchIDs[i]
+
+            try:
+                Match.objects.get(
+                    match_id=mid,
+                    region=region,
+                    version=version,
+                    gamemode=gamemode
+                )
+                print "~ skipped"
+                continue
+            except:
+                pass
+
+            r = requests.get(
+                API_BASE_URL + "{region}/v2.2/match/{mid}?api_key={key}&includeTimeline=true".format(
+                    region=region.lower(),
+                    mid=mid,
+                    key=API_KEY
+                )
+            )
+
+            if r.status_code is not 200:
+                had_errors = True
+                print "~ ERROR {s_code}".format(s_code=r.status_code)
+                errorMatches.append(mid)
+                continue
+
+            try:
+                data = r.json()
+                Match(
+                    match_id=data['matchId'],
+                    region=data['region'],
+                    version=version,
+                    gamemode=gamemode,
+                    data=r.text
+                ).save()
+                print
+            except Exception as e:
+                had_errors = True
+                print "~ " + str(e)
+                errorMatches.append(mid)
+                continue
+
+        matchIDs = errorMatches
+
 # from apps.functions.Pull import *
 # getDevData()
 def getDevData():
-    downloadData(5.11, 'NORMAL_5X5', 'NA')
-    downloadData(5.14, 'NORMAL_5X5', 'NA')
+    #downloadData2(5.11, 'NORMAL_5X5', 'NA')
+    downloadData2(5.14, 'NORMAL_5X5', 'NA')
