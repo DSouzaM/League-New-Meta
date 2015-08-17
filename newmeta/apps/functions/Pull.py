1aa35a439b4171f13128f14b4110189ac8db07b4
Dongyu Zheng <garydzheng@gmail.com>
2015-08-16 23:41:43 -0400

    better

--- newmeta/apps/functions/Pull.py
+++ newmeta/apps/functions/Pull.py
@@ -23,55 +23,6 @@ def downloadData(version, gamemode, region):
     region = region.upper()
 
     matchIDs = getMatchIDs(version, gamemode, region)
-    numMatches = len(matchIDs)
-
-    had_errors = True
-
-    while had_errors:
-
-        had_errors = False
-
-        for i in range(numMatches):
-
-            print "Processing match {i} / {total}".format(i=i,total=numMatches),
-
-            mid = matchIDs[i]
-
-            try:
-                Match.objects.get(match_id=mid,region=region)
-                print "~ skipped"
-                continue
-            except:
-                pass
-
-            r = requests.get(
-                API_BASE_URL + "{region}/v2.2/match/{mid}?api_key={key}&includeTimeline=true".format(
-                    region=region.lower(),
-                    mid=mid,
-                    key=API_KEY
-                )
-            )
-
-            if r.status_code is not 200:
-                had_errors = True
-                print "~ ERROR {s_code}".format(s_code=r.status_code)
-                continue
-
-            try:
-                data = r.json()
-                Match(match_id=data['matchId'],region=data['region'],version=version,gamemode=gamemode,data=r.text).save()
-                print
-            except Exception as e:
-                had_errors = True
-                print "~ " + str(e)
-                continue
-
-def downloadData2(version, gamemode, region):
-
-    gamemode = gamemode.upper()
-    region = region.upper()
-
-    matchIDs = getMatchIDs(version, gamemode, region)
 
     while matchIDs:
         print matchIDs
@@ -128,5 +79,5 @@ def downloadData2(version, gamemode, region):
 # from apps.functions.Pull import *
 # getDevData()
 def getDevData():
-    #downloadData2(5.11, 'NORMAL_5X5', 'NA')
-    downloadData2(5.14, 'NORMAL_5X5', 'NA')
+    downloadData(5.11, 'NORMAL_5X5', 'NA')
+    downloadData(5.14, 'NORMAL_5X5', 'NA')
