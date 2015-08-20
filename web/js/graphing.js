6c9dff2f9413de5a5f332a7508cd74c52b03d8f1
Matt D'Souza <matt.dsouza@gmail.com>
2015-08-20 01:01:23 -0400

    Fixed up the checkboxes, tested and improved function to get data set

--- web/js/graphing.js
+++ web/js/graphing.js
@@ -2,7 +2,55 @@ var dataTypes = ['CHAMPIONS','ITEMS'];
 var queueTypes = ['NORMAL_5X5', 'RANKED_SOLO'];
 var regions = ['BR', 'EUNE', 'EUW', 'KR', 'LAN', 'LAS', 'NA', 'OCE', 'RU', 'TR']
 
+// object to keep track of what data should be shown on the graph
+var selection = {'data':'','queue':[], 'region':[] };
+
 $(function() {
+// function to handle checkbox actions
+$('input[type=checkbox]').change(function() {
+	var name = this.name;
+	var value = this.value;
+
+		//check action
+		if ($(this).attr('checked')){
+			if (value == 'ALL') {
+				//selects all checkboxes with the given name, checks them, and adds them to the according selection array
+				$('input[type=checkbox][name=' + name + ']').each(function() {
+					$(this).attr('checked',true);
+					if (this.value != 'ALL' && selection[this.name].indexOf(this.value) < 0) {
+						selection[this.name].push(this.value);
+					}
+				});
+			} else {
+				//pushes value to according selection array
+				selection[name].push(value);
+			}
+		}
+
+		//uncheck action
+		else {
+			if (value == 'ALL') {
+				//selects all checkboxes with the given name, unchecks them, and removes them from the according selection array
+				$('input[type=checkbox][name=' + name + ']').each(function() {
+					$(this).attr('checked',false);
+					if (selection[this.name].indexOf(this.value) >= 0) {
+						selection[this.name].splice(selection[this.name].indexOf(this.value),1);
+					}
+				});
+			} else {
+				//removes value from according selection array
+				selection[name].splice(selection[name].indexOf(value),1);
+				$('input[type=checkbox][name=' + name + '][value=ALL]').attr('checked',false);
+			}
+		}
+
+		//TODO add graph button and later add functionality for other data types
+		selection['data'] = 'CHAMPIONS';
+		getDataSet(selection);
+	});
+
+
+	/*
 	var startTime = new Date().getTime();
 	var request = $.getJSON('json/NORMAL_5X5_NA.json');
 	var chart;
@@ -14,7 +62,7 @@ $(function() {
 		}
 		console.log(champs);
 		chart = $('#container').highcharts();
-		/*champs = champs.sort(sortDescend);*/
+		champs = champs.sort(sortDescend);
 		
 		console.log('done in ' + (new Date().getTime()-startTime) + ' ms');
 
@@ -47,9 +95,10 @@ $(function() {
 				valueSuffix: '%'
 			}
 		});
-	});
+}); */
 });
 
+//function used by Array.sort to sort arrays of data objects in descending order
 function sortDescend(a,b) {
 	if (a.data[0] > b.data[0]) {
 		return -1;
@@ -60,18 +109,38 @@ function sortDescend(a,b) {
 	}
 }
 
-function getDataSet(dataType, queueList, regionList) {
-	if (dataTypes.indexOf(dataType.toUpperCase())<0 || !queueList.every(function(queue){
-		return (queueTypes.indexOf(queue.toUpperCase())>=0);
-	}) || !regionList.every(function(region) {
-		return (regions.indexOf(region.toUpperCase())>=0);
+
+
+function getDataSet(selection) {
+
+	//input sanitation
+	var dataType = selection['data'].toUpperCase()
+	var queueList = [];
+	$.each(selection['queue'], function(index, queue){
+		queueList.push(queue.toUpperCase());
+	})
+	var regionList = [];
+	$.each(selection['region'], function(index, region) {
+		regionList.push(region.toUpperCase());
+	})
+
+	//input validation
+	if (dataType=='' || dataTypes.indexOf(dataType)<0 || queueList.length==0 ||  !queueList.every(function(queue){
+		return (queueTypes.indexOf(queue)>=0);
+	}) || regionList.length==0 || !regionList.every(function(region) {
+		return (regions.indexOf(region)>=0);
 	})) {
-		console.log("Invalid data set request:\ndataType: " + dataType + "\nqueueList: " + queueList + "\nregionList: " + regionList);
+		console.log('Invalid data set request:\ndataType: ' + dataType + '\nqueueList: ' + queueList + '\nregionList: ' + regionList);
 	}
 	else {
-		console.log("Valid data set request.");
+
+	//compiles array of required JSON files
+	var jsons = [];
+	for (var i = 0; i < queueList.length; i++) {
+		for (var j = 0; j < regionList.length; j++) {
+			jsons.push(dataType+'_'+queueList[i]+'_'+regionList[j]+'.json');
+		}
 	}
-
-	//var jsons = [];
-
+	console.log(jsons);
+}
 }
