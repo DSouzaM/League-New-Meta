6f4cfe9ff65d0dbf5a834d1a7e3836cdfcab6d74
Gary Zheng <gary.zheng@n-dimension.com>
2015-08-21 14:58:21 -0400

    did some things

--- newmeta/functions/iterate.py
+++ newmeta/functions/iterate.py
@@ -3,19 +3,27 @@ from functions.util import *
 import json
 
 
-def getChampionWinLoss(version, region):
+def generateChampionWinLoss(version, region):
 
     gamemode = 'NORMAL_5X5'
     region = region.upper()
     assert(assertVersionGamemodeRegion(version=version,region=region))
 
-    matches = Match.objects.filter(region__name=region,version__name=version,gamemode__name=gamemode)
+    region_object = Region.objects.get(name=region)
+    version_object = Version.objects.get(name=version)
+    gamemode_object = Gamemode.objects.get(name=gamemode)
+
+    matches = Match.objects.filter(
+        region=region_object,
+        version=version_object,
+        gamemode=gamemode_object
+    )
     total = matches.count()
 
     Champion.objects.filter(
-        region__name=region,
-        version__name=version,
-        gamemode__name=gamemode
+        region=region_object,
+        version=version_object,
+        gamemode=gamemode_object
     ).update(wins=0,picks=0,bans=0)
 
     for i in xrange(total):
@@ -35,7 +43,12 @@ def getChampionWinLoss(version, region):
             champ_id = player['championId']
             team_id = player['teamId']
 
-            champ = Champion.objects.get(key=champ_id,region__name=region,version__name=version,gamemode__name=gamemode)
+            champ = Champion.objects.get(
+                key=champ_id,
+                region=region_object,
+                version=version_object,
+                gamemode=gamemode_object
+            )
             
             champ.picks += 1
             
