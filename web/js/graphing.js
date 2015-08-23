4806a547cbc90768bf05fb0e19b5638694d6593c
DSouzaM <matt.dsouza@gmail.com>
2015-08-23 18:29:55 -0400

    Implementing new JSON format, more modular functions

--- web/js/graphing.js
+++ web/js/graphing.js
@@ -53,10 +53,10 @@ $('input[type=checkbox]').change(function() {
 
 var startTime = new Date().getTime();
 var request = $.getJSON('json/NORMAL_5X5_NA.json');
-var champs = [];
+var dataSet = [];
 
 request.done(function(data) {
-	champs = data;
+	dataSet = data;
 	$('#container').highcharts({
 		'chart': {
 			'type': 'columnrange',
@@ -67,7 +67,7 @@ request.done(function(data) {
 		},
 
 		'xAxis': {
-			'categories': getCategories(champs),
+			'categories': getArrayOf(dataSet,'name'),
 			'min': 0
 		},
 
@@ -81,7 +81,7 @@ request.done(function(data) {
 		},
 		
 		'series': [{
-			'data': getWinSeries(champs)
+			'data': setDataSet(dataSet,'prepost','wr')//getSeries(dataSet,'')
 		}],
 		'tooltip': {
 			'formatter': function() {
@@ -94,20 +94,76 @@ request.done(function(data) {
 			}
 		}
 	});
-	console.log('Initialization done in ' + (new Date().getTime()-startTime) + ' ms.');
+console.log('Initialization done in ' + (new Date().getTime()-startTime) + ' ms.');
 }); 
 
 $('#btn').click(function() {
 	//sample sort with time taken
 	var start = new Date().getTime();
-	champs = champs.sort(sortByProperty('wins'));
+	dataSet.sort(sortByProperty('d_wr'));	
 	var chart = $('#container').highcharts();
-	chart.xAxis[0].setCategories(getCategories(champs));
-	chart.series[0].setData(getWinSeries(champs));
+	chart.xAxis[0].setCategories(getArrayOf(dataSet,'name'));
 	chart.series[0].update(chart.series[0].options);
 	console.log('Update done in ' + (new Date().getTime()-start) + ' ms.');
 });
+$('#block').css({'width':'200px','height':'200px','background-color':'red'});
+$('#slider').on('input',function() {
+	$('#block').css('background-color',getColor($(this).val(),0,100));	
 });
+});
+
+// UNTESTED 
+//infoType = 'prepost', 'change'
+//info = 'wr','pr','br'
+//sets Point properties of each champion object. low/high for columnrange graph, y for column
+function setDataSet(dataSet, infoType, info) {
+	var range = getRange(dataSet,'d_'+info);
+	var min = range[0];
+	var max = range[1];
+	for (var i = 0; i < dataSet.length; i++) {
+		if (dataSet[i]['d_'+info] > 0) {// rate increases
+			dataSet[i].low = dataSet[i]['pre_'+info]; 
+			dataSet[i].high = dataSet[i]['post_'+info];
+			//dataSet[i].color = 'green';
+		} else { // rate decreases
+			dataSet[i].low = dataSet[i]['post_'+info];
+			dataSet[i].high = dataSet[i]['pre_'+info];
+			//dataSet[i].color = 'red';
+		}
+		dataSet[i].currentInfo = info;
+		dataSet[i].y = dataSet[i]['d_'+info];
+		//console.log(dataSet[i].y,min,max);
+		dataSet[i].color = getColor(dataSet[i].y,min,max);
+		//console.log(dataSet[i].color);			
+	}
+	return dataSet;
+}
+
+
+function getColor(value, min, max) { 
+	value -= min;
+	var range = max-min;
+	var green = Math.round(255*(value/range));
+	var red = Math.round(255*(1.0-(value/range)));
+	return 'rgb('+red+','+green+',0)';
+}
+
+
+function getRange(dataSet,property){
+	var low = dataSet[0][property];
+	var high = dataSet[0][property];
+	for (var i = 0; i < dataSet.length; i++){
+		var value = dataSet[i][property];
+		if (value < low) {
+			low = value;
+		} else if (value > high) {
+			high = value;
+		}
+	}
+	return [low,high];
+}
+
+
 
 // returns a callback function for Arrays.sort which will sort by one of the object's properties
 function sortByProperty(property) {
@@ -121,35 +177,45 @@ function sortByProperty(property) {
 	}
 }
 
-function getCategories(champs) {
-	var categories = [];
-	for (var i = 0; i < champs.length; i++) {
-		categories.push(champs[i].name);
+function getArrayOf(dataSet,property){
+	var array = [];
+	for (var i = 0; i < dataSet.length; i++){
+		array.push(dataSet[i][property])
 	}
-	return categories;
+	return array;
 }
 
 function roundOff(num) {
 	return parseFloat((Math.round(num*100)/100).toFixed(2));
 }
 
-function getWinSeries(champs) {
+
+// may be removed. requires regeneration of entire series. if modification of variables works through setDataSet() this can probably be deleted
+//infoType = 'prepost', 'change'
+//info = 'wr','pr','br'
+function getSeries(dataSet, infoType, info) {
 	var series = [];
-	for (var i = 0; i < champs.length; i++) {
-		var pre = roundOff(champs[i]['5.11']['wins']/champs[i]['5.11']['picks']*100);
-		var post = roundOff(champs[i]['5.14']['wins']/champs[i]['5.14']['picks']*100);
-		if (pre < post) {
-			series.push({
-				'low':pre, 'high':post, 'color':'green'
-			});
-		} else if (pre > post) {
-			series.push({
-				'low':post, 'high':pre, 'color':'red'
-			});
-		} else {
-			series.push({
-				'low':pre, 'high':post, 'color':'grey'
-			})
+	if (true/*infoType == 'prepost'*/) {
+		for (var i = 0; i < dataSet.length; i++) {
+			var pre = roundOff(dataSet[i]['5.11']['wins']/dataSet[i]['5.11']['picks']*100);
+			var post = roundOff(dataSet[i]['5.14']['wins']/dataSet[i]['5.14']['picks']*100);
+			if (pre < post) {
+				series.push({
+					'low':pre, 'high':post, 'color':'green'
+				});
+			} else if (pre > post) {
+				series.push({
+					'low':post, 'high':pre, 'color':'red'
+				});
+			} else {
+				series.push({
+					'low':pre, 'high':post, 'color':'grey'
+				})
+			}
+		}
+	} else if (infoType == 'change') {
+		for (var i = 0; i < dataSet.length; i++) {
+			series.push(dataSet['d_'+infoType]);
 		}
 	}
 	return series
