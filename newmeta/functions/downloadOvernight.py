3ffdc40f203133dbcc96996f3b0a56058d83023e
Dongyu Zheng <garydzheng@gmail.com>
2015-08-23 23:36:02 -0400

    ye

--- newmeta/functions/downloadOvernight.py
+++ newmeta/functions/downloadOvernight.py
@@ -4,14 +4,14 @@ from pull import *
 
 def download():
 
-    getMatchData(5.11, 'NORMAL_5x5', 'NA')
-    getMatchData(5.14, 'NORMAL_5x5', 'NA')
+    getMatchData(5.11, 'RANKED_SOLO', 'NA')
+    getMatchData(5.14, 'RANKED_SOLO', 'NA')
 
-    getChampions(5.11, 'NORMAL_5x5', 'NA')
-    getChampions(5.14, 'NORMAL_5x5', 'NA')
+    getChampions(5.11, 'RANKED_SOLO', 'NA')
+    getChampions(5.14, 'RANKED_SOLO', 'NA')
 
-    getIteration(8, 5.11, 'NORMAL_5x5', 'NA')
-    getIteration(8, 5.14, 'NORMAL_5x5', 'NA')
+    getIteration(8, 5.11, 'RANKED_SOLO', 'NA')
+    getIteration(8, 5.14, 'RANKED_SOLO', 'NA')
 
-    count_champ_normal_5x5(5.11, 'NA')
-    count_champ_normal_5x5(5.14, 'NA')
+    # count_champ_normal_5x5(5.11, 'NA')
+    # count_champ_normal_5x5(5.14, 'NA')
