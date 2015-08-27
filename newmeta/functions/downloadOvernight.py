98960807bfad06853a645b342d4512e6ff593abe
Gary Zheng <garydzheng@gmail.com>
2015-08-27 00:37:41 -0400

    fix

--- newmeta/functions/downloadOvernight.py
+++ newmeta/functions/downloadOvernight.py
@@ -13,10 +13,11 @@ def download():
     for region in regions:
         for queue in queues:
             for version in versions:
+
                 getMatchData(version, queue, region)
                 getChampions(version, queue, region)
                 getIteration(version, queue, region)
                 generateChampionRoles(version, queue, region)
-
-            count_champ(version, queue, region)
+                count_champ(version, queue, region)
+            
             exportChampions(queue, region)
