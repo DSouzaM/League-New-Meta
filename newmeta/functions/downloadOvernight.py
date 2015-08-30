ef0ee942db6826e479363b947373cbfc8e330e89
Gary Zheng <garydzheng@gmail.com>
2015-08-30 00:08:51 -0400

    added roles to datatable

--- newmeta/functions/downloadOvernight.py
+++ newmeta/functions/downloadOvernight.py
@@ -12,12 +12,12 @@ def download():
 
     for region in regions:
         for queue in queues:
-            #for version in versions:
+            for version in versions:
 
                 #getMatchData(version, queue, region)
                 #getChampions(version, queue, region)
                 #getIteration(8, version, queue, region)
-                #generateChampionRoles(version, queue, region)
+                generateChampionRoles(version, queue, region)
                 #count_champ(version, queue, region)
             
             exportChampions(queue, region)
